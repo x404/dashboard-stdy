34d737057c335920ef2741dca682b5ac2e06636f
x404 <kronos2003@gmail.com>
2024-09-09 17:31:44 +0300

    feat: use destroyRef instead of ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,22 +7,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
-    setInterval( () => {
+    // this.interval = setInterval( () => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
-      if (rnd < 0.5){
+      if (rnd < 0.5) {
         this.currentStatus = 'online';
-      } else if (rnd < 0.9){
+      } else if (rnd < 0.9) {
         this.currentStatus = 'offline';
       } else {
         this.currentStatus = 'unknown';
       }
-    }, 5000 );
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    })
+  }
+
+  ngOnDestroy() {
+    // clearTimeout(this.interval);
   }
 
 }
