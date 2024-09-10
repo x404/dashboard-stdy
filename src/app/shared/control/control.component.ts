60f7ea0672be073da89accaf66116c3112b883d3
x404 <kronos2003@gmail.com>
2024-09-10 18:31:09 +0300

    feat: add contentChild signal for form

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,13 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component, contentChild,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -11,20 +20,26 @@ import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '
     class: 'control',
 
     // recommended by Angular command
-    // '(click)': 'onClick()',
+    '(click)': 'onClick()',
   }
 })
 export class ControlComponent {
   // similar on host in decorator
   // @HostBinding('class') className = 'control';
 
-  @HostListener('click') onClick() {
-    console.log('Clicked!')
-  }
-
-  label = input.required();
-  //
-  // onClick(){
+  // @HostListener('click') onClick() {
   //   console.log('Clicked!')
   // }
+
+  label = input.required<string>();
+  private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  //
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  onClick(){
+    console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control());
+  }
 }
