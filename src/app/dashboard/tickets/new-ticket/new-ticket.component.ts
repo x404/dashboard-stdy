e499eaf6b4f32a9d5779f0134e4a05f791f6a7c5
x404 <kronos2003@gmail.com>
2024-09-10 18:20:29 +0300

    feat: add viewChild signal for form

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule,
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  constructor() {
+  }
+
+
+  onSubmit(title: string, text: string): void {
+    console.log('submitted');
+    this.form()?.nativeElement.reset();
+  }
 }
